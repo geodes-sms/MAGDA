2857458c5a9c38da4a28362d7738c285d87f3f25
meriembenchaaben <meriembenchaaben97@gmail.com>
2023-04-20 11:16:01 -0400

    New project layout

--- plugins/ca.umontreal.geodes.merriem.cdeditor.editor/src/ca/umontreal/geodes/merriem/cdeditor/editor/JobConcepts.java
+++ plugins/ca.umontreal.geodes.merriem.cdeditor.editor/src/ca/umontreal/geodes/merriem/cdeditor/editor/JobConcepts.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -184,7 +185,9 @@ public class JobConcepts extends Job {
 
 			}
 		});
-
+		if (monitor.isCanceled()) {
+			return Status.CANCEL_STATUS;
+		}
 		this.cancel();
 		return ASYNC_FINISH;
 
---
+++ tests/ca.umontreal.geodes.merriem.cdeditor.editor.test/src/ca/umontreal/geodes/merriem/cdeditor/editor/test/ServicesTests.java
@@ -0,0 +1,17 @@
+package ca.umontreal.geodes.merriem.cdeditor.editor.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+
+import ca.umontreal.geodes.merriem.cdeditor.editor.*;
+
+import org.junit.jupiter.api.Test;
+
+class ServicesTests {
+
+	@Test
+	void test() {
+	}
+
+}
